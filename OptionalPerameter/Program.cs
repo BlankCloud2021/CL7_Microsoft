432986223ff1158c60830b99b10f43403a44d096
BlankCloud2021 <78029266+BlankCloud2021@users.noreply.github.com>
2021-02-25 09:13:46 -0500

    Add files via upload
    Demonstrating Optional Parameters effect on a function

---
+++ OptionalPerameter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OptionalPerameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UserInfo called with no perameters");
+            Optional.userInfo();
+
+            Console.WriteLine();
+
+            Console.WriteLine("UserInfo called with both perameters");
+            Optional.userInfo("Carl",9);
+                
+
+        }
+
+
+
+    class Optional
+        {
+            //Question 1 
+            static public void userInfo(string name = "No Name", int age = 0)
+            {
+                Console.WriteLine($"The Adventure begins with {name}. your character age is {age}");
+            }
+            //Question 2 
+            //Overloaded becuse is now expecting a double for age instead of an int.
+            static public void userInfo(string name, double age)
+            {
+                Console.WriteLine($"The Adventure begins with {name}. your character age is {age}");
+            }
+
+
+        }
+    }
+}
