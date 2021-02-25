19be6d9783729edf9eb6f7760849dc2855aaae03
BlankCloud2021 <78029266+BlankCloud2021@users.noreply.github.com>
2021-02-25 09:02:21 -0500

    Add files via upload
    A Math Calculator to perform equations.

---
+++ MathFormula/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathFormula
+{
+    class SecondClass
+    {
+
+        //Method for Finding Circumference
+        //Input Pass Radius
+        public double circ(double i)
+        {
+            double ans = 2 * Math.PI * i;
+            i = ans;
+            return i;
+
+        }
+
+        //Method for Finding Area of Circle
+        public double AreaC(double i)
+        {
+            double ans = Math.PI * (i * i);
+            i = ans;
+            return i;
+
+        }
+        //Method for Finding Volume of hemisphere
+        public double Vol(double i)
+        {
+            double ans = ((4 / 3) * Math.PI * (i * i * i)) / 2;
+            i = ans;
+            return i;
+        }
+        //Method for Finding area of Triangle 
+        public double AreaT(double a, double b, double c)
+        {
+            //*Try and create an IF (a,b,c) greater than zero
+            double p = (a + b + c) / 2;
+            Double ans = Math.Sqrt((p * (p - a)) * (p - b) * (p - c));
+            double i = ans;
+            return i;
+
+
+        }
+        //Method for Solving Quadatic equation
+        //make sure values are valid (b^2 > 4ac)
+        public double Quad1(double a, double b, double c)
+        {
+            //double ans = (-b) + Math.Sqrt((b * b) - (4 * (a * c))) / (2 * a);
+
+            double sqrtpart = (b * b) - (4 * a * c);
+            double ans = ((-1) * b + Math.Sqrt(sqrtpart)) / (2 * a);
+            double i = ans;
+            return i;
+        }
+        //Quadratic Equation
+        public double Quad2(double a, double b, double c)
+        {
+            //double ans = (-b) + Math.Sqrt((b * b) - (4 * (a * c))) / (2 * a);
+
+            double sqrtpart = (b * b) - (4 * a * c);
+            double ans = ((-1) * b - Math.Sqrt(sqrtpart)) / (2 * a);
+            double i = ans;
+            return i;
+        }
+        //tuple example
+
+        //Quadratic Equation As a tuple 
+        /*
+         public(double, double) Quad2(double a, double b, double c)
+           {
+               //double ans = (-b) + Math.Sqrt((b * b) - (4 * (a * c))) / (2 * a);
+
+               double sqrtpart = (b * b) - (4 * a * c);
+               double ans = ((-1) * b - Math.Sqrt(sqrtpart)) / (2 * a);
+               double ans2= ((-1) * b + Math.Sqrt(sqrtpart)) / (2 * a);
+               return (ans, ans2);
+           }
+        */
+    }
+
+}
+
+
---
+++ MathFormula/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using MathFormula;
+namespace MathFormula
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Create a new object of the SecondClass
+            //keyword formula creates shortcut to the SecondClass
+            //use for loop to take user input
+            MathFormula.SecondClass formula = new SecondClass();
+            double r,a,b,c,x;
+            double[] Num = new double[5];
+
+            Console.WriteLine("Enter your integers For Radius, A,B,C,and X");
+            Console.Write("in that Order:");
+            for(int i=0;i<Num.Length; i++)
+            {
+                Console.WriteLine("-----------------------------------------------------------");
+                Num[i] = Double.Parse(Console.ReadLine());
+
+            }
+            //Assign the array to the Variables
+            r = Num[0];
+            a = Num[1];
+            b = Num[2];
+            c = Num[3];
+            x = Num[4];
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.ReadKey();
+
+            //State the action and add the method
+            Console.WriteLine("The Circumefrence is:");
+            double Ans = formula.circ(r);
+            Console.WriteLine(Ans);
+
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.ReadKey();
+
+            Console.WriteLine("The Area of the circle is: ");
+            double Ans2 = formula.AreaC(r);
+            Console.WriteLine(Ans2);
+
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.ReadKey();
+
+            Console.WriteLine("The volume of the hemisphere is: ");
+            double Ans3 = formula.Vol(r);
+            Console.WriteLine(Ans3);
+
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.ReadKey();
+
+            Console.WriteLine("The Area of the triangle is: ");
+            double Ans4 = formula.AreaT(a, b, c);
+            Console.WriteLine(Ans4);
+
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.ReadKey();
+
+            //Insert Quadratic Fourmula
+            Console.WriteLine("The quadratic formula is: ");
+            double Ans5 = formula.Quad1(a, b, c);
+            double Ans6 = formula.Quad2(a, b, c);
+            Console.WriteLine($"{Ans5} and {Ans6}" );
+            
+            
+            Console.WriteLine("-------------------------------------------------------------");
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+}
